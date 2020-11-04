a2670b42e2cc7e10cd4fb6fb5b7b08ecc9917887
VismayRaul <vismayraul@gmail.com>
2020-11-04 22:44:47 +0530

    store in dictionary

--- uc.sh
+++ uc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+declare -A dict
+
 read a
 read b
 read c
@@ -12,3 +14,6 @@ compute2=$(($a*$b+$c))
 compute3=$(($c+$a/$b))
 
 compute4=$(($a%$b+$c))
+
+dict=([1]=$compute1 [2]=$compute2 [3]=$compute3 [4]=$compute4)
+echo ${dict[@]}
